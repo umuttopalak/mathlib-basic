1de3b676b495b08c3d50f1297a6ab3b8bea49713
Umut Topalak <umuttopalak@hotmail.com>
2024-11-30 02:56:32 +0300

    feat: rename package to mathlib_basic

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='mathlib',
+    name='mathlib_basic',
     version='0.1',
     packages=find_packages(),
     description='A Python library for basic mathematical operations',
